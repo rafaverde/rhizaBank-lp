afdef85258a0c12b31f0c31272b3d6613c9d3569
Rafael Valverde <rafaverde@msn.com>
2025-03-27 16:02:56 -0300

    update: Faq's links with buttons implemented.

--- src/components/faq-box.tsx
+++ src/components/faq-box.tsx
@@ -5,10 +5,16 @@ import {
   AccordionTrigger,
 } from "@radix-ui/react-accordion";
 import { BiChevronDown } from "react-icons/bi";
+import { Button } from "./button";
+import Link from "next/link";
 
 interface Faq {
   ask: string;
   answer: string;
+  button?: {
+    link: string;
+    label: string;
+  };
 }
 
 interface FaqBoxProps {
@@ -34,14 +40,22 @@ export function FaqBox({ faqs }: FaqBoxProps) {
                 <h3>{faq.ask}</h3>
                 <BiChevronDown className="transition-transform duration-300 ease-in-out group-data-[state=open]:rotate-180" />
               </AccordionTrigger>
-              <AccordionContent className="data-[state=closed]:animate-slideUp data-[state=open]:animate-slideDown h-max overflow-hidden bg-rhiza-green-300 px-4 text-rhiza-green-900">
-                <div className="py-4">
+              <AccordionContent className="h-max overflow-hidden bg-rhiza-green-300 px-12 text-rhiza-green-900 data-[state=closed]:animate-slideUp data-[state=open]:animate-slideDown">
+                <div className="py-8">
                   {faq.answer.split("\n").map((line, index) => (
                     <p key={index} className="mt-2 first:mt-0">
                       {line}
                     </p>
                   ))}
                 </div>
+
+                {faq.button ? (
+                  <Link target="_blank" href={faq.button.link}>
+                    <Button className="mb-10 rounded-lg border border-rhiza-green-900 bg-transparent px-8 py-3 font-bold uppercase text-rhiza-green-900 transition-colors hover:bg-rhiza-green-900 hover:text-rhiza-green-500 lg:flex">
+                      {faq.button.label}
+                    </Button>
+                  </Link>
+                ) : null}
               </AccordionContent>
             </AccordionItem>
           );
